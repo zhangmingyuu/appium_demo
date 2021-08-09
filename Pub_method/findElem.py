b5fa2d907ccb9de24c0742421e4e7325dcc88805
zhangmingyuu <578716326@qq.com>
2021-08-09 16:49:32 +0800

    新增了通过坐标定位的方式

--- Pub_method/findElem.py
+++ Pub_method/findElem.py
@@ -104,3 +104,52 @@ class FindElem:
                 self.driver.press_keycode(keycode=i)
             else:
                 self.driver.press_keycode(keycode=i[0], metastate=i[1], flags=i[2])
+
+    # 滑动屏幕的方法
+    def swipe_screen(self, pre_width, pre_height, end_width, end_height):
+        """
+        :param pre_width: 滑动起点横坐标
+        :param pre_height: 滑动起点纵坐标
+        :param end_width: 滑动终点横坐标
+        :param end_height: 滑动终点纵坐标
+        :return: 无返回值
+        """
+        # 基本的分辨率，用来计算相对坐标系数的，这个值采取的是市场主流屏幕的分辨率
+        basic_width = 1080
+        basic_height = 2134
+
+        # 获取当前手机的分辨率
+        size = self.driver.get_window_size()
+        current_width = size['width']
+        current_height = size['height']
+
+        # 计算系数，这里还有待完善，按说横坐标和纵坐标的系数只计算一次就可以，这里计算了两次
+        width = round(pre_width / basic_width, 2)
+        height = round(pre_height / basic_height, 2)
+        end_wi = round(end_width / basic_width, 2)
+        end_hei = round(end_height / basic_height, 2)
+
+        self.driver.swipe(width * current_width, height * current_height, end_wi * current_width,
+                          current_height * end_hei)
+
+    # 通过坐标点击元素的方法
+    def coordinate_by_tap(self, width, height, duration=None):
+        """
+        :param width: 要点击的元素的横坐标
+        :param height: 要点击的元素的纵坐标
+        :param duration: 点击持续的时间，如果时间长了，就是长按
+        :return: 无返回值
+        """
+        basic_width = 1080
+        basic_height = 2134
+
+        # 获取当前手机的分辨率
+        size = self.driver.get_window_size()
+        current_width = size['width']
+        current_height = size['height']
+
+        # 计算相对坐标的系数，并保留两位小数
+        x = round(width / basic_width, 2)
+        y = round(height / basic_height, 2)
+
+        self.driver.tap([(current_width * x, current_height * y)], duration)
